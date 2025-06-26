557420762d419c722dad92b11bafd915057130b5
benjamin <benjaminoheneasare65@gmail.com>
2025-06-26 23:54:08 +0000

    changes

--- administrator/urls.py
+++ administrator/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('administrator/', views.dashboard, name='administrator')
+    path('administrator/', views.dashboard, name='administrator'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('report/', views.report, name='report'),
+    path('income_expenses/', views.income_expenses, name='income_expenses'),
+    path('stockHistory/', views.stockHistory, name='stockHistory'),
 ]
